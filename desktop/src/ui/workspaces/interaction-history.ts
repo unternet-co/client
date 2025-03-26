cbb3768b29b92c34a0b98aed083f48213322f87a
Rupert Manfredi <mail@ruperts.world>
2025-03-26 13:04:46 -0700

    Added markdown, moved style assets to ui/common

--- desktop/src/ui/workspaces/interaction-history.ts
+++ desktop/src/ui/workspaces/interaction-history.ts
@@ -1,5 +1,6 @@
 import { html, render, TemplateResult } from 'lit';
-import { Interaction, InteractionOutput } from '../../models/interactions';
+import { InteractionOutput } from '../../models/interactions';
+import { resolveMarkdown } from 'lit-markdown';
 import { appendEl, createEl } from '../../utils/dom';
 import { Workspace, WorkspaceModel } from '../../models/workspaces';
 import { dependencies } from '../../base/dependencies';
@@ -48,9 +49,11 @@ class InteractionHistory extends HTMLElement {
   outputTemplate(output: InteractionOutput) {
     let template: TemplateResult = html``;
     if (output.type === 'text') {
-      template = html`${output.content}`;
+      template = html`${resolveMarkdown(output.content)}`;
     }
-    return html`<div class="interaction-output">${template}</div>`;
+    return html`<div class="interaction-output" data-format="markdown">
+      ${template}
+    </div>`;
   }
 }
 
