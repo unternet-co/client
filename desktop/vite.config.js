76c586b276fe853c75f80e669c7d3f33912e76c5
vincent <vincentcharlebois@gmail.com>
2025-03-29 18:51:21 -0400

    fix,format

--- desktop/vite.config.js
+++ desktop/vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from "vite";
-import path from "path";
+import { defineConfig } from 'vite';
+import path from 'path';
 
 export default defineConfig({
-  base: './', // This is important for the static folder to be served correctly
   publicDir: 'static',
   envDir: path.resolve(__dirname, '..'),
   envPrefix: 'APP_',
