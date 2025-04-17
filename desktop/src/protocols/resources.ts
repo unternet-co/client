cc21051a309cf56cbd10bc9af1093013c8f37a53
Rupert Manfredi <mail@ruperts.world>
2025-04-17 15:26:05 -0700

    Working on protocols

---
+++ desktop/src/protocols/resources.ts
@@ -0,0 +1,33 @@
+import { Resource } from '@unternet/kernel';
+import webResources from './web/resources';
+
+const initialResources: Array<Resource> = new Array();
+
+if (import.meta.env.APP_UNTERNET_API_KEY) {
+  initialResources.push(...webResources);
+}
+
+interface ResourceModelInit {
+  initialResources: Array<Resource>;
+}
+
+class ResourceModel {
+  readonly resources: Array<Resource>;
+
+  constructor({ initialResources }: ResourceModelInit) {
+    this.resources = initialResources;
+  }
+
+  find(uri: string) {
+    let result: Resource;
+    result = this.resources.find((x) => x.uri === uri);
+
+    if (!result) {
+      throw new Error(`No resource matches this URI: ${JSON.stringify(uri)}`);
+    }
+
+    return result;
+  }
+}
+
+export { ResourceModel, initialResources };
---
+++ desktop/src/protocols/web/protocol.ts
@@ -0,0 +1,22 @@
+import Unternet from '@unternet/sdk';
+import { ActionDirective, Protocol } from '@unternet/kernel';
+
+class UnternetProtocol implements Protocol {
+  readonly scheme = 'unternet';
+  connection = new Unternet({
+    apiKey: import.meta.env.APP_UNTERNET_API_KEY,
+    isDev: import.meta.env.DEV,
+  });
+
+  async handler(directive: ActionDirective) {
+    if (directive.actionId === 'search') {
+      const results = await this.connection.lookup.query({
+        q: directive.args.q,
+      });
+      console.log(results);
+      return results;
+    }
+  }
+}
+
+export const unternetProtocol = new UnternetProtocol();
