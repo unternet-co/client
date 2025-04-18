95e3ec2a1b221b520f423d069d3b192e17737d9f
Steven Vandevelde <steven@tokono.ma>
2025-04-18 16:18:41 +0200

    doc: Some code documentation for the kernel

--- kernel/src/actions/protocols.ts
+++ kernel/src/actions/protocols.ts
@@ -1,5 +1,14 @@
 import { ActionDirective } from './actions';
 
+/**
+ * Protocols determine how an `ActionDirective` is executed.
+ *
+ * This goes hand in hand with `Resource`s.
+ * Each protocol has a unique `scheme`.
+ *
+ * {@link ActionDirective}
+ * {@link Resource}
+ */
 export interface Protocol {
   scheme: string;
   handler: ProtocolHandler;
@@ -9,6 +18,12 @@ export type ProtocolHandler = (
   directive: ActionDirective
 ) => any | Promise<any>;
 
+/**
+ * Creates a protocol-handlers dictionary indexed by the protocol scheme.
+ *
+ * @param protocols
+ * @returns Protocol dictionary/record/map.
+ */
 export function createProtocolHandlers(protocols: Protocol[]) {
   const handlers: Record<string, ProtocolHandler> = {};
   for (const protocol of protocols) {
