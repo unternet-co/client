90ff07403e74266119d2de3bb7d2030db806878a
vincent <vincentcharlebois@gmail.com>
2025-04-18 13:29:25 -0400

    fix: update Vite configuration for production base path

--- desktop/vite.config.js
+++ desktop/vite.config.js
@@ -4,6 +4,7 @@ import path from 'path';
 const isDev = process.env.NODE_ENV !== 'production';
 
 export default defineConfig({
+  base: isDev ? '/' : './',
   envDir: path.resolve(__dirname, '..'),
   envPrefix: 'APP_',
   build: {
