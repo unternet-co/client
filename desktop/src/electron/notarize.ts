9211b4c82d1c042c2c5e02d286b4b2a22e2302a3
Vinay Raghu <hello@vinayraghu.com>
2025-04-18 19:06:24 -0400

    change notarize to use APPLE_APP_SPECIFIC_PASSWORD

--- desktop/src/electron/notarize.ts
+++ desktop/src/electron/notarize.ts
@@ -22,7 +22,7 @@ export default async function notarizing(context: AfterPackContext) {
   return notarize({
     appPath: `${appOutDir}/${appName}.app`,
     appleId: process.env.APPLE_ID,
-    appleIdPassword: process.env.APPLE_ID_PASS,
+    appleIdPassword: process.env.APPLE_APP_SPECIFIC_PASSWORD,
     teamId: process.env.APPLE_TEAM_ID,
   });
 }
