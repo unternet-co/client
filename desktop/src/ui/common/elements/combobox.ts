b2d2178c60a89ac24e02e630f180ed9cfd4b78d4
Vinay Raghu <hello@vinayraghu.com>
2025-04-27 21:28:45 -0400

    feat: add keyboard navigation using shortcut service

--- desktop/src/ui/common/elements/combobox.ts
+++ desktop/src/ui/common/elements/combobox.ts
@@ -1,5 +1,7 @@
 import { LitElement, html, css } from 'lit';
 import { KernelInput } from '../../../ai/kernel';
+import { ShortcutService } from '../../../shortcuts/shortcut-service';
+import { dependencies } from '../../../common/dependencies';
 
 export class ComboboxSelectEvent extends Event {
   input: KernelInput;
@@ -22,19 +24,67 @@ export class ComboboxElement extends LitElement {
   };
 
   options: { label: string; value: string }[] = [];
-  selectedValue: string | undefined;
+  selectedValue: string | null;
   searchString: string;
+  selectedIndex: number = 0;
+  private shortcutService =
+    dependencies.resolve<ShortcutService>('ShortcutService');
 
   constructor() {
     super();
     this.selectedValue = this.selectedValue || this.options?.[0]?.value;
     this.onSelect = this.onSelect.bind(this);
     this.searchString = this.searchString || '';
+    this.selectNextOption = this.selectNextOption.bind(this);
+    this.selectPrevOption = this.selectPrevOption.bind(this);
+    this.selectOption = this.selectOption.bind(this);
+    this.closeOptions = this.closeOptions.bind(this);
+    this.registerShortcuts();
   }
 
   private onSelect(value: string) {
     this.selectedValue = value;
     this.dispatchEvent(new ComboboxSelectEvent(value));
+  }
+
+  selectNextOption() {
+    this.selectedIndex = (this.selectedIndex + 1) % this.options.length;
+    this.selectedValue = this.options[this.selectedIndex].value;
+  }
+
+  selectPrevOption() {
+    this.selectedIndex =
+      (this.selectedIndex - 1 + this.options.length) % this.options.length;
+    this.selectedValue = this.options[this.selectedIndex].value;
+  }
+
+  selectOption() {
+    const selectedOption = this.options[this.selectedIndex];
+    if (selectedOption) {
+      this.onSelect(selectedOption.value);
+    }
+  }
+
+  closeOptions() {
+    this.selectedValue = null;
+    this.searchString = '';
+    // this.emitClose();
+  }
+
+  registerShortcuts() {
+    this.shortcutService.register('ArrowDown', this.selectNextOption);
+    this.shortcutService.register('ArrowUp', this.selectPrevOption);
+    this.shortcutService.register('Enter', this.selectOption);
+    this.shortcutService.register('Tab', this.selectOption);
+    this.shortcutService.register('Escape', this.closeOptions);
+  }
+
+  deregisterShortcuts() {
+    this.shortcutService.deregister('ArrowDown', this.selectNextOption);
+    this.shortcutService.deregister('ArrowUp', this.selectPrevOption);
+    this.shortcutService.deregister('Enter', this.selectOption);
+    this.shortcutService.deregister('Tab', this.selectOption);
+    this.shortcutService.deregister('Escape', this.closeOptions);
   }
 
   render() {
@@ -65,6 +115,16 @@ export class ComboboxElement extends LitElement {
         list-style: none;
         padding: 0;
         margin: 0;
+      }
+
+      .combobox-option {
+        padding: 8px;
+        cursor: pointer;
+      }
+      .combobox-option:hover,
+      .combobox-option.selected {
+        background-color: var(--color-action-800);
+        color: var(--color-action-0);
       }
     `;
   }
